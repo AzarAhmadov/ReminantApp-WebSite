dc7aec793f8a80946c1ba12b12a435966e32a749
AzarAhmadov <82292818+AzarAhmadov@users.noreply.github.com>
2021-12-04 11:59:37 +0400

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,12 +9,82 @@ acardion.forEach((item) => {
     });
   });
 
-
   const open_bar = document.querySelector('.open-btn');
   const mobil_list = document.querySelector('.listMobil');
   const Body = document.querySelector('#body');
   open_bar.addEventListener('click', () => {
-    mobil_list.classList.toggle('active')
-    Body.classList.toggle('active')
-  })
+    mobil_list.classList.toggle('active');
+    Body.classList.toggle('active');
+});
   
+
+window.sr = new ScrollReveal()
+ScrollReveal({
+  reset: false
+});
+
+sr.reveal('.banner-left', {
+  origin: "top",
+  distance: "-300px",
+  delay: 400
+})
+
+sr.reveal('.banner-right', {
+  origin: "bottom",
+  distance: "-300px",
+  delay: 400
+})
+
+sr.reveal('.logo', {
+  scale: 0.8,
+  interval: 200
+})
+
+sr.reveal('.app-top-img', {
+  origin: "left",
+  distance: "300px",
+  delay: 400
+})
+
+sr.reveal('.span', {
+  origin: "top",
+  distance: "200px",
+  delay: 400
+})
+
+sr.reveal('.anime', {
+  origin: "top",
+  distance: "200px",
+  delay: 400
+})
+
+sr.reveal('.right-item', {
+  origin: "left",
+  distance: "200px",
+  delay: 400
+})
+
+sr.reveal('.emer-left', {
+  duration: 1500
+})
+
+sr.reveal('.emer-right', {
+  duration:1500
+})
+
+sr.reveal('.card-flex', {
+  scale: 0.8,
+  interval: 200
+})
+
+sr.reveal('.coment', {
+  origin:"top",
+  distance: "100px",
+  interval: 200
+})
+
+sr.reveal('.last-item', {
+  origin:"top",
+  distance: "100px",
+  interval: 200
+})
